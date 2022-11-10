9faed2cbde6b756c58f5202e38a9d07906e16200
Tharaka Sandaruwan <tharaka.maths@gmail.com>
2022-11-10 22:59:25 +0530

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { useState } from "react";
+
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className='w-full flex py-6 justify-between items-center navbar' >
+
+    </nav>
+  ) 
 };
 
 export default Navbar;
