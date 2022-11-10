2d1b774d12b43b77ff08788f1a74a6399f5fc862
Tharaka Sandaruwan <tharaka.maths@gmail.com>
2022-11-10 22:05:45 +0530

    add_components

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Billing from "./Billing";
+import Business from "./Business";
+import Navbar from "./Navbar";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import Stats from "./Stats";
+import Hero from "./Hero";
+import Testimonials from "./Testimonials";
+
+export {
+  Billing,
+  Business,
+  Navbar,
+  CardDeal,
+  Clients,
+  CTA,
+  Footer,
+  Stats,
+  Hero,
+  Testimonials,
+};
