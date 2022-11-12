25ace7f149100f653a30640f48391182ed1f2e8f
Tharaka Sandaruwan <tharaka.maths@gmail.com>
2022-11-12 20:04:29 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
           <li
             key={nav.id}
             className={`font-poppins font-normal text-[16px] text-white ${
-              navLinks === navLinks.length - 1 ? "mr-0" : "mr-5"
+              navLinks === navLinks.length - 1 ? "mr-0" : "mr-10"
             } hover:text-cyan-300`}
           >
             <a href={`#${nav.id}`}>{nav.title}</a>
@@ -27,7 +27,32 @@ const Navbar = () => {
       </ul>
 
       <div className="sm:hidden flex flex-1 justify-end items-center">
-        <img src={toggle ? close : menu} />
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle((prev) => !prev)}
+        />
+
+        {/* Mobile Menu */}
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute right-0 top-20 mx-4 my-2 min-w-[150px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex flex-col flex-1 justify-end items-center">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal text-[16px] text-white ${
+                  navLinks === navLinks.length - 1 ? "mr-0" : "mb-3"
+                } hover:text-cyan-300`}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
   );
