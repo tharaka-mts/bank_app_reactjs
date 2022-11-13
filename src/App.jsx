78e6bb332f49da40e9c246957fcc3a2095127424
Tharaka Sandaruwan <tharaka.maths@gmail.com>
2022-11-13 23:51:58 +0530

    Business Section

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ const App = () => (
     </div>
 
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth} text-white`}>
+      <div className={`${styles.boxWidth}`}>
         <Stats /> 
         <Business />
         <Billing /> 
