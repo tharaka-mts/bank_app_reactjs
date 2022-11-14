27f3ae35aa361eb50fcd687f0514c8cc045e87db
Tharaka Sandaruwan <tharaka.maths@gmail.com>
2022-11-14 23:59:38 +0530

    CardDeal Section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { feedback } from '../constants'
+import styles from '../style'
+import FeedBackCard from './FeedBackCard'
 
-const Testimonials = () => {
-  return (
-    <div>Testimonials</div>
+const Testimonials = () => (
+    <section id='clients'>
+
+    </section>
   )
-}
 
 export default Testimonials
