f721d040da84536e76012134197b4496f0f2984c
Tharaka Sandaruwan <tharaka.maths@gmail.com>
2023-12-14 02:09:00 +0530

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // ** https://vitejs.dev/config/ **
 export default defineConfig({
+  base: "/bank_app_reactjs/",
   plugins: [react()],
-  // Enable Hosting  
+  // Enable Hosting
   // server: {
   //   host: true
   // }
